a28a114c082581995448ddc54934dc070aa6cb11
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2021-09-22 09:44:08 +0200

    models

--- trip_czech/main/models.py
+++ trip_czech/main/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.db.models.enums import Choices
+from multiselectfield import MultiSelectField
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+ROOM_TYPES = (
+    ('1', 'Jednolůžkový'),
+    ('2', 'Dvojlůžkový'),
+    ('3', 'Trojlůžkový'),
+    ('4', 'Čtyřlůžkový'),
+)
+
+PARTS_OF_REPUBLIC = (
+    ('CZE', 'Čechy'),
+    ('MOR', 'Morava'),
+    ('SLZ', 'Slezsko'),
+)
+
+REGIONS = (
+    ('JČ', 'Jihočeský'),
+    ('PRG', 'Praha'),
+    ('STŘ', 'Středočeský'),
+    ('VYS', 'Vysočina'),
+    ('JHM', 'Jihomoravský'),
+    ('OLM', 'Olomoucký'),
+    ('PAR', 'Pardubický'),
+    ('LIB', 'Liberecký'),
+)
+
+
+class Accomodation(models.Model):
+    name = models.CharField(max_length=255)
+    link_to_gmaps = models.CharField(max_length=999)
+    stars = models.IntegerField(default=1,
+                                validators=[
+                                    MaxValueValidator(5),
+                                    MinValueValidator(1)
+                                ])
+    room_types = MultiSelectField(choices=ROOM_TYPES)
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    part_of_republic = models.CharField(max_length=255,
+                                        choices=PARTS_OF_REPUBLIC, default="CZE")
+
+
+region = models.CharField(max_length=255,
+                          choices=REGIONS, default="none")
+
+
+class Inquiry(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    gdpr = models.BooleanField(default=False)
+    # end of step one - COMMIT!
--- trip_czech/trip_czech/settings.py
+++ trip_czech/trip_czech/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'multiselectfield',
+
     'main.apps.MainConfig',
 ]
 
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'trip_czech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'trip_czech',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
